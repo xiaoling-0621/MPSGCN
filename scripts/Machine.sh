a482b81554354b8d4b8537602d90c026205f2d9e
xiaoling-0621 <xl13751191120@gmail.com>
2026-02-01 02:27:09 +0800

    Add files via upload
    hyperparameters of RF, SVR, and Ridge

---
+++ scripts/Machine.sh
@@ -0,0 +1,47 @@
+'''
+model = RF
+area = BOHAI
+n_estimators=100,
+max_depth=None,
+min_samples_split=5,
+min_samples_leaf=1,
+max_features = "sqrt"
+
+area = ECS
+n_estimators=200,
+max_depth=10,
+min_samples_split=5,
+min_samples_leaf=2,
+max_features = 0.5
+
+area = SCS
+n_estimators=200,
+max_depth=10,
+min_samples_split=5,
+min_samples_leaf=2,
+max_features = 0.5
+'''
+
+'''
+model = SVR
+area = BOHAI
+C=1,
+epsilon=0.1,
+kernel=‘linear’
+
+area = ECS
+C=10,
+epsilon=0.1,
+kernel=‘linear’
+
+area = SCS
+C=1,
+epsilon=0.01,
+kernel=‘linear’。
+'''
+
+'''
+model = Ridge
+alpha=10
+'''
+
